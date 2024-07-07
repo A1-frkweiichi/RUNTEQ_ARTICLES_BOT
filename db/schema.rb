d9c987f8fc0c09a62be3c2a172aee66e39d41983
furukawaeiichi <engineer.frkw@gmail.com>
2024-07-07 17:30:04 +0900

    add app/models/article.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_113354) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_063148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "source_platform", null: false
+    t.string "external_id", null: false
+    t.string "title", null: false
+    t.string "article_url", null: false
+    t.datetime "published_at", null: false
+    t.integer "likes_count", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["external_id"], name: "index_articles_on_external_id", unique: true
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "mattermost_id", null: false
@@ -27,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_113354) do
     t.index ["zenn_username"], name: "index_users_on_zenn_username"
   end
 
+  add_foreign_key "articles", "users"
 end
