fba495b2d41cb3db341d609aeb4a948ff9e05f59
furukawaeiichi <engineer.frkw@gmail.com>
2024-08-03 01:04:20 +0900

    Delete WakeUpJob for WorkerDyno.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,6 @@ require 'rspec/rails'
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 
-require 'sidekiq/testing'
-Sidekiq::Testing.fake!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
