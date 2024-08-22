e275fbec2fdcf6006f878b4b0e2ba91a9a9047b3
furukawaeiichi <engineer.frkw@gmail.com>
2024-08-22 23:53:05 +0900

    メモリ状況の確認

--- config/initializers/schedule_national_holiday_jobs.rb
+++ config/initializers/schedule_national_holiday_jobs.rb
@@ -1,14 +1,14 @@
-Rails.application.config.after_initialize do
-  scheduled_dates = [
-    DateTime.new(2024, 9, 16, 3, 0, 0),
-    DateTime.new(2024, 9, 23, 3, 0, 0),
-    DateTime.new(2024, 10, 14, 3, 0, 0),
-    DateTime.new(2024, 11, 4, 3, 0, 0),
-    DateTime.new(2024, 12, 30, 3, 0, 0),
-    DateTime.new(2024, 12, 31, 3, 0, 0)
-  ]
+# Rails.application.config.after_initialize do
+#   scheduled_dates = [
+#     DateTime.new(2024, 9, 16, 3, 0, 0),
+#     DateTime.new(2024, 9, 23, 3, 0, 0),
+#     DateTime.new(2024, 10, 14, 3, 0, 0),
+#     DateTime.new(2024, 11, 4, 3, 0, 0),
+#     DateTime.new(2024, 12, 30, 3, 0, 0),
+#     DateTime.new(2024, 12, 31, 3, 0, 0)
+#   ]
 
-  scheduled_dates.each do |date|
-    PostToXJob.set(wait_until: date).perform_later
-  end
-end
+#   scheduled_dates.each do |date|
+#     PostToXJob.set(wait_until: date).perform_later
+#   end
+# end
